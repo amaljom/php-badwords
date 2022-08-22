f530faedcc9753939f6ed610e13d4987ee34b5dd
amaljom <amaljom99@gmail.com>
2022-08-22 15:43:41 +0200

    the variable has been declared and the its been visualized

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <title>Bad words</title>
 </head>
 <body>
-    
+    <h1>bad words</h1>
+    <?php 
+        $text='ciao questo è il testo che ho scritto'
+    ?>
+    <h2>questa è la frase da modificare: <?php echo $text ?></h2>
 </body>
 </html>
